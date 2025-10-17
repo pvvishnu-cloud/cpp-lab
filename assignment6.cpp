c0df925a67115bf2de128ae24babf8a41110e4b4
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 10:47:06 +0530

    Create assignment6.cpp

---
+++ assignment6.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class TelephoneBill {
+private:
+    string name;
+    string address;
+    string telephoneNo;
+    int numberOfCalls;
+
+public:
+    
+    TelephoneBill(string n, string addr, string tel, int calls) {
+        name = n;
+        address = addr;
+        telephoneNo = tel;
+        numberOfCalls = calls;
+    }
+
+    friend void calculateBill(TelephoneBill &tb);
+};
+
+void calculateBill(TelephoneBill &tb) {
+    const int chargePerCall = 2;
+    int amount = tb.numberOfCalls * chargePerCall;
+
+    cout << "\n----- Telephone Bill -----" << endl;
+    cout << "Name: " << tb.name << endl;
+    cout << "Address: " << tb.address << endl;
+    cout << "Telephone No.: " << tb.telephoneNo << endl;
+    cout << "Number of Calls: " << tb.numberOfCalls << endl;
+    cout << "Amount to be paid: Rs. " << amount << endl;
+}
+int main() {
+    string name, address, telephoneNo;
+    int numberOfCalls;
+    cout << "Enter Name: ";
+    getline(cin, name);
+    cout << "Enter Address: ";
+    getline(cin, address);
+    cout << "Enter Telephone Number: ";
+    getline(cin, telephoneNo);
+    cout << "Enter Number of Calls: ";
+    cin >> numberOfCalls;
+    TelephoneBill tb(name, address, telephoneNo, numberOfCalls);
+    calculateBill(tb);
+    return 0;
+}
