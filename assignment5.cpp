415813606d5b06c72fc5a6d083573e25e87427c7
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 11:57:33 +0530

    assignment5.cpp

--- assignment5.cpp
+++ assignment5.cpp
@@ -1,47 +1,35 @@
 #include <iostream>
 using namespace std;
-
 class AreaCalculator {
 public:
-  
     double area(double radius) {
         return 3.14159 * radius * radius;
     }
-
     double area(int side) {
         return side * side;
     }
-
     double area(double length, double width) {
         return length * width;
     }
-
     double area(double base, double height, int dummy) {
         return 0.5 * base * height;
     }
 };
-
 int main() {
     AreaCalculator calculator;
-
     double radius, length, width, base, height;
     int side;
-
     cout << "Enter radius of circle: ";
     cin >> radius;
     cout << "Area of circle: " << calculator.area(radius) << endl;
-
     cout << "Enter side of square: ";
     cin >> side;
     cout << "Area of square: " << calculator.area(side) << endl;
-
     cout << "Enter length and width of rectangle: ";
     cin >> length >> width;
     cout << "Area of rectangle: " << calculator.area(length, width) << endl;
-
     cout << "Enter base and height of triangle: ";
     cin >> base >> height;
     cout << "Area of triangle: " << calculator.area(base, height, 0) << endl;
-
     return 0;
 }
