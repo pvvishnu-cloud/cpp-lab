148bae8532a3a4a32c4e647f1fab33407316d937
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 11:42:59 +0530

    assignment2.cpp

--- assignment2.cpp
+++ assignment2.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 class Book {
 private:
     int BookID;
     string BookName;
-    float BookPrice;
-
-    
+    float BookPrice; 
     float total_cost(int n) const {
         return n * BookPrice;
     }
-
 public:
-    
     void inputDetails() {
         cout << "Enter Book ID: ";
         cin >> BookID;
-        cin.ignore();
         cout << "Enter Book Name: ";
-        getline(cin, BookName);
+        cin>> BookName;
         cout << "Enter Book Price: ";
         cin >> BookPrice;
     }
-
     void displayDetails() const {
         cout << "Book ID: " << BookID << endl;
         cout << "Book Name: " << BookName << endl;
         cout << "Book Price: " << BookPrice << endl;
     }
-
     void purchaseBook() const {
         int copies;
         cout << "Enter number of copies to purchase: ";
@@ -39,37 +31,28 @@ public:
         cout << "Total Cost to be Paid: " << cost << endl;
     }
 };
-
 int main() {
     Book book1, book2, book3;
-
     cout << "--- Enter details for Book 1 ---" << endl;
     book1.inputDetails();
     cout << endl;
-
     cout << "--- Enter details for Book 2 ---" << endl;
     book2.inputDetails();
     cout << endl;
-
     cout << "--- Enter details for Book 3 ---" << endl;
     book3.inputDetails();
     cout << endl;
-
     cout << "--- Book 1 Information ---" << endl;
     book1.displayDetails();
     book1.purchaseBook();
     cout << endl;
-
     cout << "--- Book 2 Information ---" << endl;
     book2.displayDetails();
     book2.purchaseBook();
     cout << endl;
-
-
     cout << "--- Book 3 Information ---" << endl;
     book3.displayDetails();
     book3.purchaseBook();
     cout << endl;
-
     return 0;
 }
