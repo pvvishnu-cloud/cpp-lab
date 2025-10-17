ad5d6fd34b77f58cc67d086fa3190dea5cb1a133
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 11:17:35 +0530

    Create assignment10.cpp

---
+++ assignment10.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+protected:
+    string name;
+    int age;
+
+public:
+    void getData() {
+        cout << "Enter name: ";
+        getline(cin, name);
+        cout << "Enter age: ";
+        cin >> age;
+        cin.ignore(); 
+    }
+
+    void display() const {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+class Student : public Person {
+private:
+    int roll_number;
+
+public:
+    void getStudentData() {
+        getData(); 
+        cout << "Enter roll number: ";
+        cin >> roll_number;
+    }
+
+    void showDetails() const {
+        display(); 
+        cout << "Roll Number: " << roll_number << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    cout << "--- Enter Student Details ---\n";
+    s.getStudentData();
+
+    cout << "\n--- Student Information ---\n";
+    s.showDetails();
+
+    return 0;
+}
