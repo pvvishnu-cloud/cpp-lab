7377df2ce9e65519eaa5230c1ae73b88f4c44ab2
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 11:59:45 +0530

    assignment7.cpp

--- assignment7.cpp
+++ assignment7.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-
 class Time {
 private:
     int hours;
     int minutes;
     int seconds;
-
 public:
-   
     void inputTime() {
         cout << "Enter hours: ";
         cin >> hours;
@@ -20,14 +17,11 @@ public:
     void displayTime() {
         cout << hours << " : " << minutes << " : " << seconds << endl;
     }
-
     friend Time addTime(const Time &t1, const Time &t2);
 };
-
 Time addTime(const Time &t1, const Time &t2) {
     Time result;
     int carry = 0;
-
     result.seconds = t1.seconds + t2.seconds;
     if (result.seconds >= 60) {
         result.seconds -= 60;
@@ -35,7 +29,6 @@ Time addTime(const Time &t1, const Time &t2) {
     } else {
         carry = 0;
     }
-
     result.minutes = t1.minutes + t2.minutes + carry;
     if (result.minutes >= 60) {
         result.minutes -= 60;
@@ -43,31 +36,21 @@ Time addTime(const Time &t1, const Time &t2) {
     } else {
         carry = 0;
     }
-
     result.hours = t1.hours + t2.hours + carry;
-
     return result;
 }
-
 int main() {
     Time t1, t2, sum;
-
     cout << "Enter first time:\n";
     t1.inputTime();
-
     cout << "Enter second time:\n";
     t2.inputTime();
-
     cout << "\nFirst Time: ";
     t1.displayTime();
-
     cout << "Second Time: ";
     t2.displayTime();
-
     sum = addTime(t1, t2);
-
     cout << "Sum of Times: ";
     sum.displayTime();
-
     return 0;
 }
