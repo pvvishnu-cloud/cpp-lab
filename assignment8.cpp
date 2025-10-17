e31f68fdc390c4654a093041fe94997d6bad7f4b
pvvishnu-cloud <pvvishnu546@gmail.com>
2025-10-17 12:00:42 +0530

    assignment8.cpp

--- assignment8.cpp
+++ assignment8.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 using namespace std;
-
 class Counter {
 private:
     int value;
-
 public:
-
     Counter(int v = 0) : value(v) {}
-
     void display() const {
         cout << "Value: " << value << endl;
     }
-
     Counter& operator++() {
         ++value;
         return *this;
     }
-
     Counter operator++(int) {
         Counter temp = *this; 
         value++;              
         return temp;          
     }
 };
-
 int main() {
     Counter c1(5);
 
